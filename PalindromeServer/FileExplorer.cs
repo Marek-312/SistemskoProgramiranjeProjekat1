fa8a77d3901489e399471071e510c83e4b32259d
djordjedjordjevic11 <djordje253djordjevic@gmail.com>
2026-05-08 00:19:21 +0200

    Dodata funkcija za proveru reci

--- PalindromeServer/FileExplorer.cs
+++ PalindromeServer/FileExplorer.cs
@@ -2,6 +2,36 @@ namespace PalindromeServer
 {
     internal sealed class FileExplorer
     {
-        
+        private readonly string rootDirectory;
+        private readonly LRUCache cache=new LRUCache(kapacitet:5);
+        private readonly Dictionary<string, object> inProgress=new Dictionary<string, object>();
+        private readonly object progressLock=new object();
+        private readonly Logger logger;
+        private readonly SemaphoreSlim semaphore=new SemaphoreSlim(3);
+        public FileExplorer(Logger logger)
+        {
+            this.rootDirectory=Directory.GetCurrentDirectory();
+            this.logger=logger;
+        }
+        public static bool IsPalindrome(string word)
+        {
+            string cleaned=new string(word.Where(char.IsLetter).ToArray()).ToLowerInvariant();
+            if(cleaned.Length<2)
+            {
+                return false;
+            } 
+            int left=0;
+            int right=cleaned.Length-1;
+            while(left<right)
+            {
+                if(cleaned[left]!=cleaned[right])
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
     }
 }
