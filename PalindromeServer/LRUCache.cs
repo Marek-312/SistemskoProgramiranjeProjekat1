41adca5d53b63bbf19bb36def0c53a3c692e2837
djordjedjordjevic11 <djordje253djordjevic@gmail.com>
2026-05-07 23:46:05 +0200

    Zavrsena LRUCache

--- PalindromeServer/LRUCache.cs
+++ PalindromeServer/LRUCache.cs
@@ -4,14 +4,61 @@ namespace PalindromeServer
     internal sealed class LRUCache
     {
         private readonly int kapacitet;
-        private readonly Dictionary<string, LinkedList<CacheItem>>map=new();
-        private readonly LinkedList<CacheItem>List=new();
+        private readonly Dictionary<string, LinkedListNode<CacheItem>>map=new();
+        private readonly LinkedList<CacheItem>list=new();
         private readonly object cacheLock=new object();
 
         public LRUCache(int kapacitet)
         {
             this.kapacitet=kapacitet;
         } 
+
+        public int Count
+        {
+            get{lock(cacheLock) {return map.Count;}}
+        }
+
+        public bool TryGet(string fileName, out int result)
+        {
+            lock(cacheLock)
+            {
+                if(map.TryGetValue(fileName, out var node))
+                {
+                    list.Remove(node);
+                    list.AddFirst(node);
+                    result=node.Value.PalindromCount;
+                    return true;
+                }
+            }
+            result=0;
+            return false;
+        }
+         public void Set(string fileName, int palindromeCount)
+        {
+            lock(cacheLock)
+            {
+                if(map.TryGetValue(fileName, out var existing))
+                {
+                    existing.Value.PalindromCount=palindromeCount;
+                    list.Remove(existing);
+                    list.AddFirst(existing);
+                    return;
+                }
+                if(map.Count>=kapacitet)
+                {
+                    var lru=list.Last!;
+                    list.RemoveLast();
+                    map.Remove(lru.Value.FileName);
+                    Console.WriteLine($"[KES] izbacen je {lru.Value.FileName} zbog toga sto je kes pun!");
+                }
+
+                var item=new CacheItem(fileName, palindromeCount);
+                var node=new LinkedListNode<CacheItem>(item);
+                list.AddFirst(node);
+                map[fileName]=node;
+                Console.WriteLine($"[KES] Dodat je fajl {fileName} velicina kesa je: {map.Count}/{kapacitet}");
+            }
+        }
     }
 
     internal class CacheItem
