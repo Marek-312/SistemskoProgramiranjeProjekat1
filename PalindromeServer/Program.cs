ba0435b2403584ecd8628d0be6d80c45528aa0a0
djordjedjordjevic11 <djordje253djordjevic@gmail.com>
2026-05-08 14:27:07 +0200

    Program.cs

--- PalindromeServer/Program.cs
+++ PalindromeServer/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Text;
+
+namespace PalindromeServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pokretanje servera....");
+
+            Logger logger=new Logger();
+            FileExplorer fileExplorer=new FileExplorer(logger);
+            HTTPServer server=new HTTPServer(fileExplorer);
+            server.Start(["http://localhost:5050/"]);
+        }
+    }
+}
